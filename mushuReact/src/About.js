9065f291327518e1f50d81cdfcd1875dae7b6be5
Andrew Watson <adw@Andrews-MacBook-Air.local>
2017-07-30 00:19:30 -0700

    more react changes

--- mushuReact/src/About.js
+++ mushuReact/src/About.js
@@ -1,8 +1,9 @@
 //Component
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Link, IndexLink } from 'react-router-dom'
+import { Link, IndexLink } from 'react-router-dom';
 
+//About section
 class About extends Component {
   render() {
     return (
